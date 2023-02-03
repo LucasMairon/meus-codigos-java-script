dbe7d6c1ad4581580d3825200802a0efd14fe0ae
Lucas Mairon <120278386+LucasMairon@users.noreply.github.com>
2023-02-03 10:54:00 -0300

    Add files via upload

---
+++ nome sobrenome e id.js
@@ -0,0 +1,6 @@
+let primeironome=prompt("digite o primeiro nome")
+let sobrenome=prompt("digite o seu sobrenome")
+let idade=prompt("digite sua idade")
+let id= 01
+
+console.log(`ola ${primeironome} ${sobrenome}, voce tem ${idade} anos e seu ID e ${id}`);
