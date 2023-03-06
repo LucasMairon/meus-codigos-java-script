267b07dac3eefa3a45acbc4a01ef74779b19c647
Lucas Mairon <120278386+LucasMairon@users.noreply.github.com>
2023-03-06 16:53:54 -0300

    Create usando objeto math.js

---
+++ usando objeto math.js
@@ -0,0 +1,4 @@
+let valor = prompt("digite um valor");
+let segundo_valor = prompt("digite outro valor");
+const vetor = [9, 0, 8, 1, 7, 190, 12, 87, -10, -200, 14, 11];
+console.log(`o cubo do primeiro valor é:${Math.pow(valor, 2)} a raiz do segundo valor é:${Math.sqrt(segundo_valor)} e o maior elemento do vetor pre determinado é:${Math.max(9, 0, 8, 1, 7, 190, 12, 87, -10, -200, 14, 11)}`);
