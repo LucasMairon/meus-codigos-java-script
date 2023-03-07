ac220e8daaa6a18798a9ce45d5102df7a42a03bc
Lucas Mairon <120278386+LucasMairon@users.noreply.github.com>
2023-03-07 15:07:24 -0300

    Update usando objeto math.js

--- usando objeto math.js
+++ usando objeto math.js
@@ -1,4 +1,4 @@
 let valor = prompt("digite um valor");
 let segundo_valor = prompt("digite outro valor");
 const vetor = [9, 0, 8, 1, 7, 190, 12, 87, -10, -200, 14, 11];
-console.log(`o cubo do primeiro valor é:${Math.pow(valor, 3)} a raiz do segundo valor é:${Math.sqrt(segundo_valor)} e o maior elemento do vetor pre determinado é:${Math.max(9, 0, 8, 1, 7, 190, 12, 87, -10, -200, 14, 11)} e o menor elemento do vetor pre determinado é:${Math.min(9, 0, 8, 1, 7, 190, 12, 87, -10, -200, 14, 11)}`);
+console.log(`o cubo do primeiro valor é:${Math.pow(valor, 3)} a raiz do segundo valor é:${Math.sqrt(segundo_valor)} e o maior elemento do vetor pre determinado é:${Math.max(...vetor)} e o menor elemento do vetor pre determinado é:${Math.min(...vetor)}`);
